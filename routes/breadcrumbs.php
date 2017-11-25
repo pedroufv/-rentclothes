32f3fc036ca82264ce26f57721925b04b6ac96c0
Pedro Augusto <pams.pedro@gmail.com>
2017-11-25 19:24:21 -0200

    migalhas para clientes

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -27,3 +27,26 @@ Breadcrumbs::register('products.edit', function ($breadcrumbs, $product) {
     $breadcrumbs->parent('products.index');
     $breadcrumbs->push('Produtos', route('products.edit', $product->id));
 });
+
+// Client List
+Breadcrumbs::register('clients.index', function ($breadcrumbs) {
+    $breadcrumbs->push('Clientes', route('clients.index'));
+});
+
+// Client Create
+Breadcrumbs::register('clients.create', function ($breadcrumbs) {
+    $breadcrumbs->parent('clients.index');
+    $breadcrumbs->push('Clientes', route('clients.create'));
+});
+
+// Client Show
+Breadcrumbs::register('clients.show', function ($breadcrumbs, $client) {
+    $breadcrumbs->parent('clients.index');
+    $breadcrumbs->push('Clientes', route('clients.show', $client->id));
+});
+
+// Client Edit
+Breadcrumbs::register('clients.edit', function ($breadcrumbs, $client) {
+    $breadcrumbs->parent('clients.index');
+    $breadcrumbs->push('Clientes', route('clients.edit', $client->id));
+});

8bb84ead0016b8194356f050c01afc6485b74822
Pedro Augusto <pams.pedro@gmail.com>
2017-11-25 19:35:03 -0200

    semeadores para produtos e clientes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,20 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Client::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'cpf' => $faker->regexify('[0-9]{11}'),
+        'rg' => $faker->randomNumber(8),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker $faker) {
+
+    return [
+        'description' => $faker->text(),
+        'size' => $faker->numberBetween(1, 50),
+        'price' => $faker->randomFloat(2, 100, 400),
+    ];
+});

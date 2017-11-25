66324bf77ad55e2de0e0ebdc784e1446b789fb16
Pedro Augusto <pams.pedro@gmail.com>
2017-11-25 19:50:14 -0200

    botoes para produtos e clientes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Início</div>
+                    <div class="panel-body">
+                        <div class="col-md-2">
+                            <a href="{{route('home')}}">
+                                <div class="well well-sm text-center">
+                                    <span style="font-size: 55px;" class="glyphicon glyphicon-home"></span>
+                                    <p>Início</p>
+                                </div>
+                            </a>
                         </div>
-                    @endif
-
-                    You are logged in!
+                        <div class="col-md-2">
+                            <a href="{{route('products.index')}}">
+                                <div class="well well-sm text-center">
+                                    <span style="font-size: 55px;" class="glyphicon glyphicon-briefcase"></span>
+                                    <p>Produtos</p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="{{route('clients.index')}}">
+                                <div class="well well-sm text-center">
+                                    <span style="font-size: 55px;" class="glyphicon glyphicon-user"></span>
+                                    <p>Clientes</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection

e9cc0d5ef4a06fe4f40eb5c9516aac2c98fd0b5f
Pedro Augusto <pams.pedro@gmail.com>
2017-11-25 22:14:08 -0200

    centralizando botões na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,8 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Início</div>
-                    <div class="panel-body">
-                        <div class="col-md-2">
+                    <div class="panel-body col-md-offset-2">
+                        <div class="col-md-3">
                             <a href="{{route('products.index')}}">
                                 <div class="well well-sm text-center">
                                     <span style="font-size: 55px;" class="glyphicon glyphicon-briefcase"></span>
@@ -15,7 +15,7 @@
                                 </div>
                             </a>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-3">
                             <a href="{{route('clients.index')}}">
                                 <div class="well well-sm text-center">
                                     <span style="font-size: 55px;" class="glyphicon glyphicon-user"></span>
@@ -23,7 +23,7 @@
                                 </div>
                             </a>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-3">
                             <a href="{{route('rents.index')}}">
                                 <div class="well well-sm text-center">
                                     <span style="font-size: 55px;" class="glyphicon glyphicon-shopping-cart"></span>

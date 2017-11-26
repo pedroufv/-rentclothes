3ceca7b77cf353ad2f89676cf32e5f11a746b368
Samuel Arcanjo <samuel.arcanjoo@hotmail.com>
2017-11-26 19:37:13 -0300

    Model de Addresses

--- app/Address.php
+++ app/Address.php
@@ -3,26 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Address extends Model
 {
-  protected $fillable = ['client_id', 'user_id','funcionario_id','street', 'number', 'zip_code','neighborhood','city','state'];
+  protected $fillable = ['client_id', 'user_id','funcionario_id','street', 'number', 'complement', 'zip','district','city','state'];
   protected $dates = ['created_at', 'updated_at', 'deleted_at'];
-
-
-  public function client()
-  {
-      return $this->belongsTo(Client::class);
-  }
-
-  public function user()
-  {
-      return $this->belongsTo(User::class);
-  }
-
-  public function products()
-  {
-      return $this->belongsToMany(Product::class);
-  }
 
 }
